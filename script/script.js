bf994b59f1a8667d628c95e0f9295751f232e36a
Roman Savich <63207808+romacherep@users.noreply.github.com>
2023-03-03 22:40:03 +0300

    Delete script.js

--- script/script.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello, World')
-const input = document.getElementById('myInput')
-const urlSearchParams = new URLSearchParams(window.location.search)
-const params = Object.fromEntries(urlSearchParams.entries());
-input.innerText=params.code
-console.log(params)
