14a7424127c017af3902ebeb4a67e3fb82281b25
Roman Savich <63207808+romacherep@users.noreply.github.com>
2023-03-03 22:47:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-let input = document.getElementById('Token')
-const urlSearchParams = new URLSearchParams(window.location.search)
-const params = Object.fromEntries(urlSearchParams.entries())
-input.value = params.code
