5e5178ddab242679fdd85e41993bdf469c981563
Roman Savich <63207808+romacherep@users.noreply.github.com>
2023-03-03 20:03:22 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,5 +2,5 @@ console.log('Hello, World')
 const input = document.getElementById('myInput')
 const urlSearchParams = new URLSearchParams(window.location.search)
 const params = Object.fromEntries(urlSearchParams.entries());
-input.value=params.code
+input.innerText=params.code
 console.log(params)
