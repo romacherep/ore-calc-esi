071d74830c08002bf3b0e01b05a27778b160f233
Roman Savich <63207808+romacherep@users.noreply.github.com>
2023-03-03 22:59:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let input = document.getElementById('Token')
 const urlSearchParams = new URLSearchParams(window.location.search)
 const params = Object.fromEntries(urlSearchParams.entries())
 console.log(params.code)
-input.value = params.code
